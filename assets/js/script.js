ab7b2712520086a4576e519d1d16ce333e10fb58
Bvivg <tatytygulovbatyrbek19@gmail.com>
2023-03-09 14:17:26 +0600

    images loaded

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let hamburger = document.querySelector('.hamburger-menu');
+let navCollapse = document.querySelector('.nav__collapse');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+  navCollapse.classList.toggle('is-active');
+});
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName(".nav__dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
